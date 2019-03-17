4016cfa26245396765a5cc57ee20e49c7edac76e
Denis Tananaev <d.d.tananaev@gmail.com>
2019-03-17 14:40:53 +0100

    functional: added the simple example for cpp api with carla

--- CppAPI/main.cpp
+++ CppAPI/main.cpp
@@ -7,10 +7,14 @@
 
 #include <carla/client/World.h>
 #include <carla/client/Client.h>
+#include <carla/rpc/WeatherParameters.h>
 #include <string>
 #include <pthread.h>
 int main(){
-
+ std::cout<<"This is minimal example of the c++ API for carla.  \
+             The program creates the client and connects to it. \
+             Afterwards it changes the weather of the simulator \
+             to heavy rain. "<<"\n";
 
  // The host and port for carla 
  std::string host = "127.0.0.1";
@@ -19,12 +23,28 @@ int main(){
  // Create carla client 
  carla::client::Client MyClient(host, port);
 
- // Add timeout
- size_t t = 10;
+ // Add timeout for 1 second to try to connect to server
+ size_t t = 1;
  auto timeout=carla::time_duration::seconds(t);
  MyClient.SetTimeout(timeout);
 
-
- std::cout<<"I made something that is working!!!!"<<"\n";
+ // Retrieve the world
+ carla::client::World MyWorld=MyClient.GetWorld();
+ // Weather parameters
+ float cloudyness = 100.0f;
+ float precipitation = 100.0f;
+ float precipitation_deposits = 100.0f;
+ float wind_intensity = 100.0f;
+ float sun_azimuth_angle = 100.5f;
+ float sun_altitude_angle = 100.5f;
+ // It is possible define weather like this
+ carla::rpc::WeatherParameters weather= carla::rpc::WeatherParameters(cloudyness,
+ 	                                                                  precipitation,
+ 	                                                                  precipitation_deposits,
+ 	                                                                  wind_intensity,
+ 	                                                                  sun_azimuth_angle,
+ 	                                                                  sun_altitude_angle);
+ // Or just use predefined settings like HardRainNoon
+ MyWorld.SetWeather(carla::rpc::WeatherParameters::HardRainNoon);
 
 }
